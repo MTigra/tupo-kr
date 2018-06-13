35cf819c268452d15aa4df483cd1da3c8811ebf6
Тигран Мартиросян <tomartirosyan@edu.hse.ru>
2018-06-14 02:57:47 +0300

    pipeeeeeeeeeeeec

---
+++ App/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using ClassLibrary1;
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square[] spisok;
+            //(1; 10) (-2; 20) 5 Piramid
+            //(2.222; 2) (3; 333) 10 Cube
+            Regex rex = new Regex(@"^\((-*\d+(?:[\.,]\d+)?);\ (-*\d+(?:[\.,]\d+)?)\)\ \((-*\d+(?:[\.,]\d+)?);\ (-*\d+(?:[\.,]\d+)?)\)\ (-*\d+(?:[\.,]\d+)?)\ (Piramid|Cube|Square)$");
+            string[] file = File.ReadAllLines("data.txt");
+            spisok = new Square[file.Length];
+            for (int i = 0; i < file.Length; i++)
+            {
+                if (!rex.IsMatch(file[i]))
+                {
+                    Console.WriteLine($"В файле обнаружена ошибка на строке {i}. Я вынуждена прекратить работать:(");
+                    return;
+                }
+
+                var match = rex.Match(file[i]);
+                var groups = match.Groups;
+                try
+                {
+
+
+                    switch (groups[5].Value)
+                    {
+                        case "Cube":
+                            {
+                                spisok[i] = new Cube(ParseDouble(groups[0].ToString()), ParseDouble(groups[1].ToString()), ParseDouble(groups[2].ToString()), ParseDouble(groups[3].ToString()), ParseDouble(groups[4].ToString()));
+                                break;
+                            }
+                        case "Piramid":
+                            {
+                                spisok[i] = new Piramid(ParseDouble(groups[0].ToString()), ParseDouble(groups[1].ToString()), ParseDouble(groups[2].ToString()), ParseDouble(groups[3].ToString()), ParseDouble(groups[4].ToString()));
+                                break;
+                            }
+                        case "Square":
+                            {
+                                spisok[i] = new Square(ParseDouble(groups[0].ToString()), ParseDouble(groups[1].ToString()), ParseDouble(groups[2].ToString()), ParseDouble(groups[3].ToString()));
+                                break;
+                            }
+
+                    }
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine($"Ошибка в строке номер {i}");
+                    return;
+                }
+            }
+
+            BinaryFormatter ser = new BinaryFormatter();
+            using (FileStream fs = new FileStream("out.bin",FileMode.Create))
+            {
+                ser.Serialize(fs,spisok);
+            }
+            Console.WriteLine("Сериализован!");
+            Square[] deSerSpisok; 
+            using (FileStream fs = new FileStream("out.bin", FileMode.Open))
+            {
+                deSerSpisok=(Square[])ser.Deserialize(fs);
+            }
+
+            for (int i = 0; i < deSerSpisok.Length; i++)
+            {
+                Console.WriteLine(deSerSpisok[i].ToString());
+            }
+
+
+        }
+
+        public static double ParseDouble(string s)
+        {
+            s = s.Replace('.', ',');
+            double d = double.Parse(s);
+            return d;
+        }
+
+
+        
+    }
+}
---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    [Serializable]
+    public class Square
+    {
+        // Props.
+        public double LowLeftX { get; set; }
+        public double LowLeftY { get; set; }
+        public double UpRightX { get; set; }
+        public double UpRightY { get; set; }
+
+        public Square(double lowLeftX,double lowLeftY, double upRightX, double upRightY)
+        {
+            LowLeftX = lowLeftX;
+            LowLeftY = lowLeftY;
+            UpRightX = upRightX;
+            UpRightY = upRightY;
+        }
+
+        // Здесь смекалка и ум. В условии сказано что они параллельны оси Ох Оу
+        // Цитата:"Square - квадрат в трехмерном пространстве, оси квадрата параллельны осям Ox, Oy."
+        public double Length
+        {
+            get { return (LowLeftY - UpRightY); }
+        }
+        public double Sq
+        {
+            get { return Length * Length; }
+        }
+
+        public virtual Square BinDeserialize(string path)
+        {
+            object obj;
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (FileStream stream = new FileStream(path, FileMode.Open))
+            {
+                obj = formatter.Deserialize(stream);
+            }
+            // либо (Square)obj тут уже сами смекалочку проявляйте
+            return obj as Square;
+        }
+
+
+        public virtual void BinSerialize(string path)
+        {
+
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (FileStream stream = new FileStream(path, FileMode.Create))
+            {
+                formatter.Serialize(stream, this);
+            }
+        }
+
+    }
+
+
+    [Serializable]
+    public class Piramid : Square
+    {
+        public Piramid(double lowLeftX, double lowLeftY, double upRightX, double upRightY,double h):base(lowLeftX,lowLeftY,upRightX,upRightY)
+        {
+            this.h = h;
+        }
+        private double h;
+
+        public double Vol
+        {
+            get { return (double)(1 / 3) * Sq * h; }
+        }
+
+        public override void BinSerialize(string path)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (FileStream stream = new FileStream(path, FileMode.Create))
+            {
+                formatter.Serialize(stream, this);
+            }
+        }
+       
+
+
+    }
+
+   public class Cube : Square
+    {
+        private double h;
+        public Cube(double lowLeftX, double lowLeftY, double upRightX, double upRightY, double h):base(lowLeftX,lowLeftY,upRightX,upRightY)
+        {
+            this.h = h;
+        }
+        /// <summary>
+        /// объем куба a*b*c. a=b=c.
+        /// </summary>
+        private double Vol
+        {
+            get { return Length * Length * Length; }
+        }
+
+        public override void BinSerialize(string path)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (FileStream stream = new FileStream(path, FileMode.Create))
+            {
+                formatter.Serialize(stream, this);
+            }
+        }
+    }
+
+    public class Figures
+    {
+        List<Square> squares = new List<Square>();
+
+        public IEnumerable Figures3d()
+        {
+            for (int i = 0; i < squares.Count; i++)
+            {
+                if (squares[i] is Cube || squares[i] is Piramid)
+                {
+                    yield return squares[i] ;
+                }
+            }
+        }
+
+        public IEnumerable MyFig(Type type)
+        {
+            
+            for (int i = 0; i < squares.Count; i++)
+            {
+                if (squares[i].GetType() == type )
+                {
+                    yield return squares[i];
+                }
+            }
+        }
+    }
+}
